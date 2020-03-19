fb6e8e40a41415693d4784f5a349063d35ff41f1
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-19 13:21:06 -0300

    comparación uso de jsx y React.createElement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = 'Patty valdebenito';
+// 5
+const jsx = (
+<div>
+    <h1>New App Badges</h1>
+    <p>Crea tu perfil ! jeje</p>
+</div>
+);
 
-// function add(){
-//     return 10;
-// }
+const constainer = document.getElementById('app');
+ReactDOM.render(jsx,constainer);
 
-const jsx = <h1>Hola, soy {name} {29+1}</h1>;
+
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1',{},'New App Badges'),
+    React.createElement('p',{},'crea tu perfil!, 2 jeje')
+);
+ReactDOM.render(element,constainer);
+
+
+
+// 4
+//const name = 'Patty valdebenito';
+/*function add(){
+     return 10;}
+*/ 
+
+//const jsx = <h1>Hola, soy {name} {29+1}</h1>;
 //const jsx1 = <h3>{add()}</h3>
 
-const container = document.getElementById("app");
-ReactDOM.render(jsx, container);
+//const container = document.getElementById("app");
+//ReactDOM.render(jsx, container);
 //ReactDOM.render(jsx1, container);
+
+/*  //3
+const element = React.createElement("a", { href:'https://portafolio-1-54bf1.firebaseapp.com/'}, "ir a portafolio");
+const container = document.getElementById("app");
+ReactDOM.render(element, container);
+ */
+
+
+/* // 2
+const element = <h1>Hello, BADGES!</h1>;
+const container = document.getElementById('app');
+ReactDOM.render(element,container);
+*/
 
 
 /* //1
@@ -23,14 +58,3 @@ const container = document.getElementById('app');
 container.appendChild(element);
 */
 
-/* // 2
-const element = <h1>Hello, BADGES!</h1>;
-const container = document.getElementById('app');
-ReactDOM.render(element,container);
-*/
-
-/*  //3
-const element = React.createElement("a", { href:'https://portafolio-1-54bf1.firebaseapp.com/'}, "ir a portafolio");
-const container = document.getElementById("app");
-ReactDOM.render(element, container);
- */
