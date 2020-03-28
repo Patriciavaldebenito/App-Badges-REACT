07e383b8a244d5afe3a46bc74ec2a7375e2ba0b3
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-28 19:25:18 -0300

    Se inicializa el estado en el formulario, pasando input de no controlado a controlado

--- src/component/BadgeForm.js
+++ src/component/BadgeForm.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./styles/BadgesForm.css";
 class BadgesForm extends React.Component {
-    handleChange=(e)=>{
-        console.log({value: e.target.value,
-                     name:e.target.name});
-    }
-    handleClick=(e) =>{
-        console.log("click!!!");
-    }
+ state={
+     jobTitle:'Designer'
+ };
 
-    handleSubmit=e=>{
-        e.preventDefault();
-        console.log("form was submit");
-    }
+  handleChange = e => {
+    // console.log({ value: e.target.value, name: e.target.name });
+    this.setState({
+        [e.target.name]:e.target.value,
+        
+    })
+  };
+  handleClick = e => {
+    console.log("click!!!");
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  console.log(this.state);
+  };
+
   render() {
     return (
       <div className="BadgesForm">
@@ -21,18 +29,64 @@ class BadgesForm extends React.Component {
           <div className="form-group">
             <label>First Name</label>
 
-            <input 
-            onChange={this.handleChange}
-            type="text" 
-            className="form-control" 
-            name="firstName"/>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="firstName"
+              value={this.state.firstName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="lastName"
+              value={this.state.lastName}
+            />
           </div>
 
-          <button 
-           
-            onClick={this.handleClick}
-            className="btn btn-primary">
-                Save
+          <div className="form-group">
+            <label>Email</label>
+
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Title</label>
+
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="twitter"
+              value={this.state.twitter}
+            />
+          </div>
+
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
           </button>
         </form>
       </div>
